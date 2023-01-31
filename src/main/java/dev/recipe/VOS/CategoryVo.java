be90178f6219df681e40a0b77b61d441f58febc0
grim0087 <wrhdqjffp12@gmail.com>
2023-01-31 11:46:48 +0900

    Changes

---
+++ src/main/java/dev/recipe/VOS/CategoryVo.java
@@ -0,0 +1,69 @@
+package dev.recipe.VOS;
+
+public class CategoryVo {
+
+    private String how;
+    private String matter;
+    private String situation;
+    private String type;
+    private String difficult;
+    private String personnel;
+    private String time;
+
+
+    public String getHow() {
+        return how;
+    }
+
+    public void setHow(String how) {
+        this.how = how;
+    }
+
+    public String getMatter() {
+        return matter;
+    }
+
+    public void setMatter(String matter) {
+        this.matter = matter;
+    }
+
+    public String getSituation() {
+        return situation;
+    }
+
+    public void setSituation(String situation) {
+        this.situation = situation;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDifficult() {
+        return difficult;
+    }
+
+    public void setDifficult(String difficult) {
+        this.difficult = difficult;
+    }
+
+    public String getPersonnel() {
+        return personnel;
+    }
+
+    public void setPersonnel(String personnel) {
+        this.personnel = personnel;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
